403ee883d012cb82b3ea2f143b0665b935288aec
shubham <shubhcool1@gmail.com>
2014-05-25 16:45:28 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('cookbookApp')
+  .controller('MainCtrl', function ($scope) {
+  	// Using AngularJS dependency injection, we've injected the $scope variable
+      // Anything we attach to scope will be available to us in the view.
+  
+      // In this case, we're attaching a collection of Awesome Things to display
+      // in app/views/main.html
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
